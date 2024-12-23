f7f477f6acd4dcdf699f7bc0f5d2ab1b0a523282
alinakassym <alinakassym@gmail.com>
2024-12-23 20:03:40 +0500

    feat: NotificationsPage

---
+++ src/pages/NotificationsPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const NotificationsPage: FC = () => {
+  return <div style={{ padding: '20px' }}>Здесь будут уведомления.</div>;
+};
+
+export default NotificationsPage;
