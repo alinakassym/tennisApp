d732592149764b923a67d174da820b3d5a20e5fa
alinakassym <alinakassym@gmail.com>
2025-01-03 20:06:14 +0500

    feat: add Tabs

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,65 @@
-import { FC } from 'react';
-
+import { FC, useState } from 'react';
+import { Button, message, Steps, theme } from 'antd';
+const steps = [
+  {
+    title: '',
+    content: '',
+  },
+  {
+    title: '',
+    content: '',
+  },
+  {
+    title: '',
+    content: '',
+  },
+];
 const MainPage: FC = () => {
-  return <div style={{ padding: '20px' }}>Добро пожаловать на Главную!</div>;
+  const { token } = theme.useToken();
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+  const contentStyle: React.CSSProperties = {
+    lineHeight: '260px',
+    textAlign: 'center',
+    color: token.colorTextTertiary,
+    backgroundColor: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: `1px dashed ${token.colorBorder}`,
+    marginTop: 16,
+  };
+  return (
+    <div className="main-page" style={{ padding: '20px' }}>
+      <Steps direction={"horizontal"} responsive={false} current={current} items={items} />
+      <div style={contentStyle}>{steps[current].content}</div>
+      <div style={{ marginTop: 24 }}>
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary" onClick={() => message.success('Processing complete!')}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+            Previous
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
